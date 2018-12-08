0165450fcbe8f5d9b45cbedae9981993e9bb2d31
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-12-08 10:52:48 -0500

    need concert fix

--- BITapi.js
+++ BITapi.js
@@ -6,21 +6,18 @@ module.exports = {
     const artist = process.argv.slice(3).join(" ");
     const bitURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
     axios.get(bitURL).then(response => {
-      response.data.length === 0
-        ? console.log(`No concerts available for ${artist}`)
-        : console.log(response);
-      // for (var i = 0; i < response.data.length; i++) {
-      //   console.log(`
-      //     LIRI has searched the Bands in Town database and has the following to report:
+      for (let i = 0; i < response.data.length; i++) {
+        console.log(` 
+        LIRI has searched the Bands in Town database and has the following to report:
 
-      //     ${artist} will be in  ${
-      //     response.data[i].venue.region
-      //   } and performing at  ${response.data[i].venue.name} on ${moment(
-      //     response.data[i].datetime
-      //   ).format("MM-DD-YYYY")}
+        ${artist} will be in  ${
+          response.data[i].venue.region
+        } and performing at  ${response.data[i].venue.name} on ${moment(
+          response.data[i].datetime
+        ).format("MM-DD-YYYY HH:MM")}
 
-      //     `);
-      // }
+        `);
+      }
     });
     //   .catch(err => console.log(`ERROR: ${err}`));
   }
